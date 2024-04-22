adb023757fc1eec7f61d44cf519b9999aa6f451f
miraxxel <forremains@yandex.ru>
2024-04-22 21:26:25 +0300

    TransferParametersByValue

---
+++ TransferParameters/main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+void Exchange(int a, int b);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int a = 2, b = 3;
+	cout << a << " " << b << endl;
+	Exchange(a, b);
+	cout << a << " " << b << endl;
+}
+
+void Exchange(int a, int b)
+{
+	int buffer = a; 
+	a = b;
+	b = buffer;
+}
