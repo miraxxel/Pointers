33e410d21c65d69bdaade7268c2db21adb3ffb1b
miraxxel <forremains@yandex.ru>
2024-04-26 21:30:57 +0300

    Split DONE

--- Split/main.cpp
+++ Split/main.cpp
@@ -1,17 +1,17 @@
 #include<iostream>
 using namespace std;
 
+void Print(int arr[], const int size);
+
 void main() 
 {
 	//setlocale(LC_ALL, "");
 	srand(time(nullptr));
 
-	int size, evenCount = 0, oddCount = 0;
+	int size;
 	cout << "Enter the size of the array: "; cin >> size;
 
-	int* arr = new int[size]; 
-	int* even = new int[size];
-	int* odd = new int[size];
+	int* arr = new int[size];
 
 	for (int i = 0; i < size; i++)
 	{
@@ -19,39 +19,52 @@ void main()
 	}
 
 	cout << "Source array: ";
+	Print(arr, size);
+
+	// 1. Calculate the number of even and odd elements
+	int evenCount = 0, oddCount = 0;
+	for (int i = 0; i < size; i++)
+	{
+		// Ternary operator
+		arr[i] % 2 == 0 ? evenCount++ : oddCount++; 
+	}
+	cout << "Count of even elements: " << evenCount << endl;
+	cout << "Count of odd elements: " << oddCount << endl;
+
+	// 2. Allocating memory for arrays
+	int* even = new int[evenCount];
+	int* odd = new int[oddCount];
+
+	// 3. Copy even and odd elements to the corresponding arrays
+	for (int i = 0, j = 0, k = 0; i < size; i++)
+	{
+		// i - counter for the source arr
+		// j - counter for the even arr
+		// k - counter for the odd arr
+		//(arr[i] % 2 == 0 ? even[j++] : odd[k++]) = arr[i];
+		// в инкриментах используется постфиксная форма, 
+		// delete единственный оператор не возвращающий значений
+		
+		(arr[i] % 2 ? odd[k++] : even[j++]) = arr[i];
+	}
+
+	// 4. Output of results
+	cout << "Even numbers: ";
+	Print(even, evenCount);
+	cout << "Odd numbers: ";
+	Print(odd, oddCount);
+	
+	// 5. Delete arrays
+	delete[] arr;
+	delete[] even;
+	delete[] odd;
+}
+
+void Print(int arr[], const int size)
+{
 	for (int i = 0; i < size; i++)
 	{
 		cout << arr[i] << " ";
 	}
 	cout << endl;
-
-	for (int i = 0; i < size; i++)
-	{
-		if (arr[i] % 2 == 0) {
-			even[evenCount] = arr[i];
-			evenCount++;
-		}
-		else {
-			odd[oddCount] = arr[i];
-			oddCount++;
-		}
-	}
-
-	cout << "Even numbers: ";
-	for (int i = 0; i < evenCount; i++)
-	{
-		cout << even[i] << " ";
-	}
-	cout << endl;
-
-	cout << "Odd numbers: ";
-	for (int i = 0; i < oddCount; i++)
-	{
-		cout << odd[i] << " ";
-	}
-	cout << endl;
-
-	delete[] arr;
-	delete[] even;
-	delete[] odd;
 }
